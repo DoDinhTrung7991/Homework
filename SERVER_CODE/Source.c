002c96d8e5587b5d2d1f681ea8ede3afc47849c0
DoDinhTrung7991 <62006129+DoDinhTrung7991@users.noreply.github.com>
2020-09-16 19:11:59 +0700

    Add files via upload
    Em nang cap code

--- SERVER_CODE/Source.c
+++ SERVER_CODE/Source.c
@@ -1,12 +1,18 @@
-#define _CRT_SECURE_NO_WARNINGS
+//#define _CRT_SECURE_NO_WARNINGS
+#define MAX_BUFFER_SIZE 256
 
 #include <stdio.h>
 #include <WinSock2.h>
 #include "Socket.h"
 
-void Send_data(char* buff, SOCKET socket_init, size_t* stop_2);
-void Receive_data(char* buf, SOCKET socket_init, size_t* stop_1);
-enum halt {T = 1, F = 0};
+SOCKET socket_init;
+size_t stop_1 = FALSE, stop_2 = FALSE;
+char buf[MAX_BUFFER_SIZE] = { 0 };
+char buff[MAX_BUFFER_SIZE] = { 0 };
+
+
+WINAPI Send_data(LPVOID lpThreadParameter);
+WINAPI Receive_data(LPVOID lpThreadParameter);
 
 /*-------------------------------------------------------------*/
 
@@ -14,41 +20,45 @@ void main()
 {	 
 	Socket_Server_Init(23);
 	listen(s, 10);
-	SOCKET socket_init = accept(s, NULL, 0);
-	char buf[256] = { 0 };
-	char buff[256] = { 0 };
+	socket_init = accept(s, NULL, 0);
 	send(socket_init, "Started communicating:", 22, 0);
-	printf("\r\n\r\nStarted communicating:");
-	while (1)
-	{
-		size_t stop_1 = F, stop_2 = F;
-		Send_data(buff, socket_init, &stop_2);
-		Receive_data(buf, socket_init, &stop_1);
-		if (stop_1 == T && stop_2 == T)
-			break;
-	}
+	printf("\r\nStarted communicating:");
+	HANDLE thread1 = CreateThread(NULL, (MAX_BUFFER_SIZE + 75), Send_data, NULL, 0, NULL);
+	HANDLE thread2 = CreateThread(NULL, (MAX_BUFFER_SIZE + 75), Receive_data, NULL, 0, NULL);
+	while ((stop_1 == FALSE) && (stop_2 == FALSE)){}
+	send(socket_init, "\r\n\r\nEnd communicating:", 21, 0);
+	printf("\r\n\r\nEnd communicating:");
 }
 
 /*-------------------------------------------------------------*/
 
-void Send_data(char* buff, SOCKET socket_init, size_t* stop_2)
+WINAPI Send_data(LPVOID lpThreadParameter)
 {
-	printf("\r\n\r\nSend: ");
-	scanf(" %[^\n]s", buff);
-	send(socket_init, "\r\n\r\n", 4, 0);
-	send(socket_init, buff, strlen(buff), 0);
+	while (1)
+	{
+		memset(buff, '\0', sizeof(buff));
+		printf("\r\n\r\nSend: ");
+		fgets(buff, MAX_BUFFER_SIZE, stdin);
+		send(socket_init, "\r\n\r\n", 4, 0);
+		send(socket_init, buff, MAX_BUFFER_SIZE, 0);
+	}
 	if (buff[0] == 'B' && buff[1] == 'y' && buff[2] == 'e' && buff[3] == '\0')
-		*stop_2 = T;
-	memset(buff, '\0', strlen(buff));
+		stop_2 = TRUE;
+	else
+		stop_2 = FALSE;
 }
 
-void Receive_data(char* buf, SOCKET socket_init, size_t* stop_1)
+WINAPI Receive_data(LPVOID lpThreadParameter)
 {
-	send(socket_init, "\r\n\r\n", 4, 0);
-	recv(socket_init, buf, 256, 0);
-	printf("\r\n\r\nReceive: %s", buf);
-	if (buf[0] == 'B' && buf[1] == 'y' && buf[2] == 'e' && buf[3] == '\0')
-		*stop_1 = T;
-	memset(buf, '\0', strlen(buf));
+	while (1)
+	{
+		memset(buf, '\0', sizeof(buf));
+		send(socket_init, "\r\n\r\n", 4, 0);
+		recv(socket_init, buf, MAX_BUFFER_SIZE, 0);
+		printf("\r\n\r\nReceive: %s\r\n", buf);
+		if (buf[0] == 'B' && buf[1] == 'y' && buf[2] == 'e' && buf[3] == '\0')
+			stop_1 = TRUE;
+		else
+			stop_1 = FALSE;
+	}
 }
-
